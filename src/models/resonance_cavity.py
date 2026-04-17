4c666ba538a3d0b7a7fe8d05be063cde168e05ad
ZenoNex <william_bryant@student.sscps.org>
2026-04-17 12:30:57 -0400

    Update resonance_cavity.py

--- src/models/resonance_cavity.py
+++ src/models/resonance_cavity.py
@@ -531,7 +531,8 @@ class ResonanceCavity(nn.Module):
         
         # Get prime frequencies: f_{p_n} = 2π·ln(p_n)
         ladder = PrimeResonanceLadder(num_resonators=num_harmonics)
-        freqs = ladder.forward().to(self.M.device)  # [num_harmonics]
+        freqs, rep_mirrors = ladder.forward()
+        freqs = freqs.to(self.M.device)  # [num_harmonics]
         
         # Derive amplitudes from cavity mode energies (evolved, not learned)
         # a_n = ||M[field_idx, n, :]|| for modes that exist, else decay
