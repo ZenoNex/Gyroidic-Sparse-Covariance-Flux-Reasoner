0c6ba8d49804cca80e77725b94cb05b9c0d48b9d
ZenoNex <william_bryant@student.sscps.org>
2026-05-12 19:11:26 -0400

    Add NonLobotomyTemporalModel and Trainer to __all__

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -2,11 +2,14 @@
 
 from .trainer import StructuralAdaptor, ConstraintDataset, collate_fn
 from .gdpo_trainer import GDPOSovereigntyAdaptor, GDPOSovereigntyPressureComputer
+from .enhanced_temporal_training import NonLobotomyTemporalModel, NonLobotomyTemporalTrainer
 
 __all__ = [
     'StructuralAdaptor',
     'ConstraintDataset',
     'collate_fn',
     'GDPOSovereigntyAdaptor',
-    'GDPOSovereigntyPressureComputer'
+    'GDPOSovereigntyPressureComputer',
+    'NonLobotomyTemporalModel',
+    'NonLobotomyTemporalTrainer'
 ]
